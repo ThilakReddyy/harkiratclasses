a58819b66da00b0de23870d0cf2601e72a3896c6
thilakreddyy <thilakreddy19102000@gmail.com>
2023-12-05 03:11:55 +0530

    First commit

---
+++ class-1/initial.js
@@ -0,0 +1,13 @@
+/*
+why languages?
+    how does computer works?
+    when you have a application locally for example vlc, zoom resides in the ssd but 
+    when you are running something it runs in the ram 
+        what exactly is in ram?
+        it is a much of 0's and 1's example binaries
+
+
+ */
+
+// Printing the hello world in javascript
+console.log("hello world");
